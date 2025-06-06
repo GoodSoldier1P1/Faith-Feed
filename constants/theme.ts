8bb62040041badb18f0dbd31849585d1705b0766
GoodSouldier1P1_7 <danielamyx859@gmail.com>
2025-06-06 19:21:07 -0400

    customized nav and colors

---
+++ constants/theme.ts
@@ -0,0 +1,11 @@
+// CHANGE ALL COLORS
+
+export const COLORS = {
+    primary: "#a60cc3",
+    secondary: "#ebd21b",
+    background: "",
+    surface: "",
+    surfaceLight: "",
+    white: "#FFFFFF",
+    grey: "#9CA3AF",
+} as const;
